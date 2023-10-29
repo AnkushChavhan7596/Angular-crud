2eb3d9c4ec78aaef990d86ff1198c25b684e9164
Ankush Chavhan <77318013+AnkushChavhan7596@users.noreply.github.com>
2023-10-29 14:56:48 +0530

    navigating exact employee for edit

--- Angular-crud/src/app/pages/home/home.component.ts
+++ Angular-crud/src/app/pages/home/home.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { Employee } from 'src/types/Employee';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
+
+   Employees : Employee[] = [
+    {
+      id : 1,
+      name : 'Rushikesh Dudhe',
+      mobile : '7028187615',
+      email : 'abc@example.com',
+      package : 4
+    },
+    {
+      id : 2,
+      name : 'Bhavesh Gugliya',
+      mobile : '7028187615',
+      email : 'abc@example.com',
+      package : 5
+    },
+    {
+      id : 3,
+      name : 'Chandan Jadhao',
+      mobile : '7028187615',
+      email : 'abc@example.com',
+      package : 6
+    },
+    {
+      id : 4,
+      name : 'Amit Bhagat',
+      mobile : '7028187615',
+      email : 'abc@example.com',
+      package : 5
+    }
+   ]
 
 }
